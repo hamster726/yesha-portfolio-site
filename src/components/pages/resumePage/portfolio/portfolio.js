00304989e64c3db35821db28056378466e4cac00
Hamster726 <114generals>
2021-08-02 19:12:47 +0300

    added new "pet" project

--- src/components/pages/resumePage/portfolio/portfolio.js
+++ src/components/pages/resumePage/portfolio/portfolio.js
@@ -1,127 +1,134 @@
 import React, {Component} from "react";
 import {
-    StyledContainer,
-    Description,
-    PortfolioRow,
-    Project,
-    ProjectImg,
-    ProjectDescription,
-    Tools,
-    Tool
+  StyledContainer,
+  Description,
+  PortfolioRow,
+  Project,
+  ProjectImg,
+  ProjectDescription,
+  Tools,
+  Tool, GitHubLink,
+  ImgContainer
 } from './style';
 import GotImage from './img/snap-react-got-wiki.jpg'
-import RestoImage from './img/snap-react-restaurant.jpg'
 import BlogImage from './img/snap-react-miniBlog.jpg'
 import CalculatorImage from './img/snap-react-calculator.png'
 import StopwatchImage from './img/snap-react-stopwatch.png'
 import RunSmartImage from './img/snap-react-runsmart.png'
+import ReactPizzaHubImg from './img/snap-react-pizzahub.png'
 
-import JSImg from './img/tools/javascript.svg'
-import ReactImg from './img/tools/react.svg'
-import ReduxImg from './img/tools/redux.svg'
-import ReactRouter from './img/tools/react-router.svg'
-import Gulp from './img/tools/gulp.svg'
-import Sass from './img/tools/sass.svg'
+import JSIco from './img/tools/javascript.svg'
+import ReactIco from './img/tools/react.svg'
+import ReduxIco from './img/tools/redux.svg'
+import ReactRouterIco from './img/tools/react-router.svg'
+import GulpIco from './img/tools/gulp.svg'
+import SassIco from './img/tools/sass.svg'
+import GithubIco from './img/tools/github-2.svg'
+import StyledComponentsIco from './img/tools/styled-components.png'
 
 
 class Portfolio extends Component {
 
-    renderProject(githubLink, demoLink, preImg, descr, js = false, react = false, redux = false, reactRouter = false, gulp = false, sass = false) {
-        return (
-            <Project>
-                <a href={githubLink} target='_blank' rel="noopener noreferrer">
-                    <ProjectImg src={preImg} alt='descr'/>
-                </a>
-                <ProjectDescription>{descr}</ProjectDescription>
-                <Tools>
-                    {js ? <Tool src={JSImg} alt='JavaSctript'/> : ''}
-                    {react ? <Tool src={ReactImg} alt='React'/> : ''}
-                    {redux ? <Tool src={ReduxImg} alt='Redux'/> : ''}
-                    {reactRouter ? <Tool src={ReactRouter} alt='React-router'/> : ''}
-                    {sass ? <Tool src={Sass} alt='Gulp'/> : ''}
-                    {gulp ? <Tool src={Gulp} alt='Gulp'/> : ''}
+  renderProject(githubLink, demoLink, preImg, descr, js = false, react = false, redux = false, reactRouter = false, gulp = false, sass = false, styledComponents = false) {
+    return (
+      <Project>
+        <ImgContainer href={demoLink} target='_blank' rel="noopener noreferrer">
+          <ProjectImg src={preImg} alt='descr'/>
+        </ImgContainer>
+        <ProjectDescription>{descr}</ProjectDescription>
+        <Tools>
+          {js ? <Tool src={JSIco} alt='JavaSctript'/> : ''}
+          {react ? <Tool src={ReactIco} alt='React'/> : ''}
+          {redux ? <Tool src={ReduxIco} alt='Redux'/> : ''}
+          {reactRouter ? <Tool src={ReactRouterIco} alt='React-router'/> : ''}
+          {sass ? <Tool src={SassIco} alt='Gulp'/> : ''}
+          {gulp ? <Tool src={GulpIco} alt='Gulp'/> : ''}
+          {styledComponents ? <Tool src={StyledComponentsIco} alt='Gulp'/> : ''}
+          {githubLink ? <GitHubLink href={githubLink} target="_blank"><Tool github src={GithubIco}/></GitHubLink> : ''}
 
 
-                </Tools>
-            </Project>
-        )
-    }
+        </Tools>
+      </Project>
+    )
+  }
 
-    render() {
-        const data = this.props.data.portfolio;
+  render() {
+    const data = this.props.data.portfolio;
 
-        return (
-            <StyledContainer fluid id='#portfolio'>
-                <Description>{data.heading}</Description>
-                <PortfolioRow xs={1} md={3}>
-                    {this.renderProject(
-                        'https://yesha-stopwatch.web.app/',
-                        null,
-                        StopwatchImage,
-                        'Stopwatch',
-                        true,
-                        true,
-                        true,
-                        true,
-                        false,
-                        true
-                    )}
-                    {this.renderProject(
-                        'https://yesha-calculator.web.app/',
-                        null,
-                        CalculatorImage,
-                        'Calculator',
-                        true,
-                        true,
-                        true,
-                        false,
-                        true
-                    )}
-                    {this.renderProject(
-                        'https://github.com/hamster726/webdev-courses/tree/master/runsmart-pulsometers_layout',
-                        null,
-                        RunSmartImage,
-                        'RunSmart pulsometer (Landing Page)',
-                        true,
-                        false,
-                        false,
-                        false,
-                        true,
-                        true
-                    )}
-
-                    {this.renderProject(
-                        'https://restaurant-app-yesha.web.app/menu/',
-                        null,
-                        RestoImage,
-                        'Restaurant app',
-                        true,
-                        true,
-                        true,
-                        true,
-                    )}
-                    {this.renderProject(
-                        'https://react-got-wiki.web.app/',
-                        null,
-                        GotImage,
-                        'Game of Thrones Wiki',
-                        true,
-                        true,
-                        false,
-                        true,
-                    )}
-                    {this.renderProject(
-                        'https://mini-blog-yesha.web.app',
-                        null,
-                        BlogImage,
-                        'TODO list',
-                        true,
-                        true,
-                    )}
-                </PortfolioRow>
-            </StyledContainer>
-        )
-    }
+    return (
+      <StyledContainer fluid id='#portfolio'>
+        <Description>{data.heading}</Description>
+        <PortfolioRow xs={1} md={3}>
+          {this.renderProject(
+            'https://github.com/hamster726/pizza-hub',
+            'https://yesha-pizza-hub.web.app/',
+            ReactPizzaHubImg,
+            'Pizza Hub',
+            true,
+            true,
+            true,
+            true,
+            false,
+            false,
+            true
+          )}
+          {this.renderProject(
+            'https://github.com/hamster726/stopwatch',
+            'https://yesha-stopwatch.web.app/',
+            StopwatchImage,
+            'Stopwatch',
+            true,
+            true,
+            true,
+            true,
+            false,
+            true
+          )}
+          {this.renderProject(
+            'https://github.com/hamster726/yesha-calculator',
+            'https://yesha-calculator.web.app/',
+            CalculatorImage,
+            'Calculator',
+            true,
+            true,
+            true,
+            false,
+            true
+          )}
+          {this.renderProject(
+            'https://github.com/hamster726/webdev-courses/tree/master/runsmart-pulsometers_layout',
+            null,
+            RunSmartImage,
+            'RunSmart pulsometer (Landing Page)',
+            true,
+            false,
+            false,
+            false,
+            true,
+            true
+          )}
+          {this.renderProject(
+            'https://github.com/hamster726/react-got-app',
+            'https://react-got-wiki.web.app/',
+            GotImage,
+            'Game of Thrones Wiki',
+            true,
+            true,
+            false,
+            true,
+          )}
+          {this.renderProject(
+            'https://github.com/hamster726/my-app',
+            'https://mini-blog-yesha.web.app',
+            BlogImage,
+            'TODO list',
+            true,
+            true,
+          )}
+        </PortfolioRow>
+      </StyledContainer>
+    )
+  }
 
 }
 
--- src/components/pages/resumePage/portfolio/style.js
+++ src/components/pages/resumePage/portfolio/style.js
@@ -1,104 +1,122 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import {Container, Row, Col} from 'reactstrap';
 
 
-
 const StyledContainer = styled(Container)`
-display: flex;
-align-items: center;
-flex-direction: column;
-min-height: 500px;
-background-color: #e9ecef;
-padding: 90px 20px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  min-height: 500px;
+  background-color: #e9ecef;
+  padding: 90px 20px;
 
-@media screen and (max-width: 768px) {
-min-height: calc(var(--vh, 1vh) * 101);
-}
+  @media screen and (max-width: 768px) {
+    min-height: calc(var(--vh, 1vh) * 101);
+  }
 `
 
 const StyledRow = styled(Row)`
-width: 100%;
-max-width: 900px;
+  width: 100%;
+  max-width: 900px;
 `
 const Description = styled.h2`
-text-align:center;
-color: #333;
-font-size: 1.75rem;
-font-weight: bold;
-margin-bottom: 40px;
+  text-align: center;
+  color: #333;
+  font-size: 1.75rem;
+  font-weight: bold;
+  margin-bottom: 40px;
 `
 
 const PortfolioRow = styled(Row)`
-display: flex;
-justify-content: space-around;
-flex-wrap: wrap;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 
 `
 const Project = styled(Col)`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-padding: 10px;
-margin: 0 10px 40px 10px;
+  padding: 10px;
+  margin: 0 10px 40px 10px;
 
-border-radius: .25rem;
-max-width: 350px;
-max-height: 500px;
-
-
-overflow:hidden;
-
-background-color: white;
-  
-transition: 0.2s;
+  border-radius: .25rem;
+  max-width: 350px;
+  max-height: 500px;
 
 
+  overflow: hidden;
 
-&:hover {
-transform: translateY(-10px);
+  background-color: white;
 
--webkit-box-shadow: 0 20px 20px 0 rgba(0,0,0,0.75);
--moz-box-shadow: 0 20px 20px 0 rgba(0,0,0,0.75);
-box-shadow: 0 20px 20px 0 rgba(0,0,0,0.75);
-}
+  transition: 0.2s;
 
-@media screen and (max-width: 768px) {
- &:hover {
-  transform: none;
-  box-shadow: none;
- }
-}
+
+  &:hover {
+    transform: translateY(-10px);
+
+    -webkit-box-shadow: 0 20px 20px 0 rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 0 20px 20px 0 rgba(0, 0, 0, 0.75);
+    box-shadow: 0 20px 20px 0 rgba(0, 0, 0, 0.75);
+  }
+
+  @media screen and (max-width: 768px) {
+    &:hover {
+      transform: none;
+      box-shadow: none;
+    }
+  }
+`
+
+const ImgContainer = styled.a`
+  width: 100%;
+  height: 336px;
 `
 
 const ProjectImg = styled.img`
-width: 100%;
+  width: 100%;
 `
-const ProjectDescription = styled.p`
-margin-top: 20px;
+const ProjectDescription = styled.h4`
+  margin-top: 20px;
+  font-size: 18px;
+  margin-bottom: 20px;
 `
 
 const Tools = styled(Row)`
-display: flex;
-width: 100%;
-margin: 0 10px;
+  display: flex;
+  width: 100%;
+  margin: 0 10px;
 `
 
 const Tool = styled.img`
-width: 30px;
-margin: 0 5px;
-height: 30px;
+  width: 30px;
+  height: 30px;
+  margin: 0 5px;
+
+  ${props => props.github && css`
+    width: 50px;
+    height: 30px;
+  `}
+`
+const GitHubLink = styled.a`
+  margin-left: auto;
+  height: 30px;
+  width: auto;
+  margin-right: 10px;
 `
 
 export {
-    StyledContainer,
-    StyledRow,
-    Description,
-    PortfolioRow,
-    Project,
-    ProjectImg,
-    ProjectDescription,
-    Tools,
-    Tool
+  StyledContainer,
+  StyledRow,
+  Description,
+  PortfolioRow,
+  Project,
+  ImgContainer,
+  ProjectImg,
+  ProjectDescription,
+  Tools,
+  Tool,
+  GitHubLink
 }
